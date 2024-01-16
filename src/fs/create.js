db42a015aa42be0b3cc0760ec0a7ef2d87c4e340
Roman Shevyakov <rs.8@bk.ru>
2024-01-16 15:06:47 +0600

    feat: complete FS part of the task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,12 @@
+import { writeFile, readdir } from "node:fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  const dirFiles = await readdir("./src/fs/files/");
+  if (dirFiles.includes("fresh.txt")) {
+    throw new Error("FS operation failed");
+  } else {
+    writeFile("./src/fs/files/fresh.txt", "I am fresh and young");
+  }
 };
 
 await create();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import { readdir, rename as nodeRename } from "node:fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+  const dirСonsistency = await readdir("./src/fs/files");
+  const isWrongFilename = dirСonsistency.includes("wrongFilename.txt");
+  const isProperFilename = dirСonsistency.includes("properFilename.md");
+
+  if (!isWrongFilename || isProperFilename) {
+    throw new Error("FS operation failed");
+  } else {
+    await nodeRename(
+      "./src/fs/files/wrongFilename.txt",
+      "./src/fs/files/properFilename.md"
+    );
+  }
 };
 
 await rename();
