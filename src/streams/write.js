6d96780117935d4c02f880c2191c362ff4620922
Roman Shevyakov <rs.8@bk.ru>
2024-01-18 09:53:39 +0600

    feat: complete Stream part of the task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createWriteStream } from "node:fs";
+
 const write = async () => {
-    // Write your code here 
+  const stream = createWriteStream("./src/streams/files/fileToWrite.txt", {
+    encoding: "utf8",
+  });
+
+  process.stdout.write(
+    "Hello!\nYou can write some data below\nPlease press Ctrl + C to exit\n"
+  );
+  process.stdin.pipe(stream);
+  process.stdin.resume();
 };
 
 await write();
